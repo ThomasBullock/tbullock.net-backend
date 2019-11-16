ab2fedfed2f10a72f7eb0999e6be433479e29a32
Thomas Bullock <motbollox@gmail.com>
2019-11-17 00:31:33 +1000

    Added brute force protection

--- auth/index.js
+++ auth/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const auth = express.Router();
 const authController = require("../controllers/authController");
 const passport = require("passport");
+const ExpressBrute = require("express-brute");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 
-auth.post("/login", authController.login); //
+let store;
+// Start slowing requests after 5 failed attempts to do something for the same user
+
+if (config.environment == "development") {
+  store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+} else {
+  // TODO handle production
+}
+const bruteforce = new ExpressBrute(store);
+
+auth.post("/login", bruteforce.prevent, authController.login); //
 auth.post("/register", authController.register);
 
 module.exports = auth;
